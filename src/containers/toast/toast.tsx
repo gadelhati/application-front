478fc6d2f53694c55a5df572ee262e785776380d
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-08-01 16:34:24 -0300

    toast

--- src/containers/toast/toast.tsx
+++ src/containers/toast/toast.tsx
@@ -2,20 +2,18 @@ import { toastInterface } from "./toast.interface";
 
 export const Toast = (toast: toastInterface) => {
     return (
-        <>
-            <div className="position-fixed bottom-0 end-0 p-3 style">
-                <div id="liveToast" className="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
-                    <div className="toast-header">
-                        {/* <img src="..." className="rounded me-2" alt="..."> */}
-                        <strong className="me-auto">Bootstrap</strong>
-                        <small>11 mins ago</small>
-                        <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                    </div>
-                    <div className="toast-body">
-                        Hello, world! This is a toast message.
-                    </div>
+        <div className="position-fixed bottom-0 end-0 p-3" style={{ zIndex: 11 }}>
+            <div id="liveToast" className="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+                <div className="toast-header">
+                    {/* <img src="..." className="rounded me-2" alt="..."> */}
+                    <strong className="me-auto">{toast.title}</strong>
+                    <small>{toast?.image}</small>
+                    <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div className="toast-body">
+                    {toast.body}
                 </div>
             </div>
-        </>
+        </div>
     );
 }
