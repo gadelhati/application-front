7ae6cf51f4acdf572bd3e650474308547d1dedae
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-08-04 13:18:13 -0300

    create all at header

--- src/containers/load/load.tsx
+++ src/containers/load/load.tsx
@@ -1,3 +1,4 @@
+import { ObservationUpload } from "../../components/observation/observation.upload";
 import { loadInterface } from "./load.interface";
 
 export const Load = (load: loadInterface) => {
@@ -8,15 +9,20 @@ export const Load = (load: loadInterface) => {
                     <h5>{load.title}</h5>
                 </div>
                 <div className="col">
-                    <button type="button" onClick={() => load.resetItem()} className="btn btn-success btn-sm float-end" data-bs-toggle="modal" data-bs-target="#modal" >Create</button>
+                    <button type="button" onClick={() => load.resetItem()} className="btn btn-success btn-sm float-end" data-bs-toggle="modal" data-bs-target="#modal" >Criar</button>
+                    {load.title == "Observações Meteorológicas" ?
+                    <ObservationUpload />
+                    :
+                    <></>
+                    }
                     {load.loading ?
                         <button className="btn btn-outline-warning btn-sm float-end" type="button" disabled>
                             <span className="spinner-border spinner-border-sm" role="status"></span>
-                            Loading
+                            Carregando
                         </button>
                         :
                         <button className="btn btn-outline-secondary btn-sm position-relative float-end" type="button" disabled>
-                            Loaded
+                            Carregado
                             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary">
                                 {load.itens}
                                 <span className="visually-hidden">unread messages</span>
