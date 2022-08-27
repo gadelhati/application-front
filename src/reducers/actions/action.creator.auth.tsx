faba5133d6edbc59dbed62def314fbb6c55a3d02
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2022-08-27 12:19:48 -0300

    window.location.href = "/application-front"

--- src/reducers/actions/action.creator.auth.tsx
+++ src/reducers/actions/action.creator.auth.tsx
@@ -12,7 +12,7 @@ export const signinAction = (object: Auth) => {
         });
         try {
             const { data } = await signin(object)
-            window.location.href = "/";
+            window.location.href = "/application-front";
             dispatch({
                 type: constants.SIGNIN_SUCCESS,
                 payload: data
