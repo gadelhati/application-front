8e0cc76d21297ad5fe8ee608b7e49a6a52963bf5
Gadelha <gadelha.ti@gmail.com>
2022-10-01 02:21:28 -0300

    header

--- src/containers/header/header.tsx
+++ src/containers/header/header.tsx
@@ -2,10 +2,8 @@ import { useDispatch } from 'react-redux';
 import { Col, Title } from "../models/content";
 import { Button } from "../models/button";
 import { headerInterface } from "./header.interface";
-import { getUserName } from "../../services/service.token"
 import { logoutAction } from '../../reducers/actions/action.creator.auth';
-import { A, AHeader } from '../menus/sidebar/sidebar.stitches'
-import person from '../../assets/image/logo/icon_person2.png'
+import { AHeader } from '../menus/sidebar/sidebar.stitches'
 import { Icon } from '../../assets/image/svg.access';
 
 export const Header = (header: headerInterface) => {
@@ -18,10 +16,7 @@ export const Header = (header: headerInterface) => {
             <h5>{header.title}</h5>
             <Col>
                 <Button float="left">
-                <AHeader href="#" data-bs-toggle="dropdown"><Icon name="people-circle" /></AHeader>
-                    {/* <a href="#" className="dropdown-toggle" data-bs-toggle="dropdown"> */}
-                        {/* <img src={person}></img> */}
-                    {/* </a> */}
+                    <AHeader href="#" data-bs-toggle="dropdown"><Icon name="people-circle" /></AHeader>
                     <ul className="dropdown-menu">
                         <li><AHeader href="#/profile">Perfil</AHeader></li>
                         <li><hr ></hr></li>
