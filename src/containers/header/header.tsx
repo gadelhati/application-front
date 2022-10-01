abd33f3e113ceefe676c359994352dc94e371999
Gadelha <gadelha.ti@gmail.com>
2022-10-01 02:18:48 -0300

    header

--- src/containers/header/header.tsx
+++ src/containers/header/header.tsx
@@ -5,6 +5,8 @@ import { headerInterface } from "./header.interface";
 import { getUserName } from "../../services/service.token"
 import { logoutAction } from '../../reducers/actions/action.creator.auth';
 import { A, AHeader } from '../menus/sidebar/sidebar.stitches'
+import person from '../../assets/image/logo/icon_person2.png'
+import { Icon } from '../../assets/image/svg.access';
 
 export const Header = (header: headerInterface) => {
     const dispatch = useDispatch();
@@ -16,9 +18,10 @@ export const Header = (header: headerInterface) => {
             <h5>{header.title}</h5>
             <Col>
                 <Button float="left">
-                    <a href="#" className="dropdown-toggle" data-bs-toggle="dropdown">
-                        <strong>{getUserName()}</strong>
-                    </a>
+                <AHeader href="#" data-bs-toggle="dropdown"><Icon name="people-circle" /></AHeader>
+                    {/* <a href="#" className="dropdown-toggle" data-bs-toggle="dropdown"> */}
+                        {/* <img src={person}></img> */}
+                    {/* </a> */}
                     <ul className="dropdown-menu">
                         <li><AHeader href="#/profile">Perfil</AHeader></li>
                         <li><hr ></hr></li>
