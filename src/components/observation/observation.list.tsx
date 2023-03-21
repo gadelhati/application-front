c9a46189444479710c7f9e63bfbd3bad500f2eb4
Gadelha TI <gadelha.ti@gmail.com>
2023-03-21 09:27:43 -0300

    add on use effect

--- src/components/observation/observation.list.tsx
+++ src/components/observation/observation.list.tsx
@@ -36,6 +36,9 @@ export const ObservationList = () => {
 
     useEffect(() => {
         retrieveAllItem()
+        stationOffShoreOptions()
+        stationOnShoreOptions()
+        observerOptions()
     }, [dispatch])
     useEffect(() => {
 
