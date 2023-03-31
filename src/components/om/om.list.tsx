310c6c0301d77a031a1ae8ee782c9be7d2f1d7d0
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2023-03-31 07:16:57 -0300

    change datatable

--- src/components/om/om.list.tsx
+++ src/components/om/om.list.tsx
@@ -51,7 +51,7 @@ export const OMList = () => {
         <Section>
             <Article>
                 <Header title={"Organizações Militares"} loading={loading} itens={itens.length} resetItem={resetItem} />
-                <DataTable itens={itens} fields={fields} selectItem={selectItem} ></DataTable>
+                <DataTable itens={itens} fields={fields} selectItem={selectItem} search={retrieveAllItem} ></DataTable>
             </Article>
             <div className="modal fade" id="modal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="ModalLabel" aria-hidden="true" >
                 <div className="modal-dialog modal-lg">
